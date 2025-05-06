8384fd7957b0724e94b6d67876ca332cc1eddca1
Lucas Farias <lucasf1@gmail.com>
2025-05-06 18:50:42 -0300

    Aulas de contexto

---
+++ curso-go/3_context/3-Key-Value/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func main() {
+
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, "token", "senha")
+	bookHotel(ctx, "Hotel")
+}
+
+func bookHotel(ctx context.Context, name string) {
+
+	token := ctx.Value("token")
+	fmt.Println(token)
+}
